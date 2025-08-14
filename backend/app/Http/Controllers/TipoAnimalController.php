46a7b58cd0da2d5ecfe42a2ebec9e035b5fa1d67
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-13 21:31:29 -0400

    Crear controlador para especie

--- backend/app/Http/Controllers/TipoAnimalController.php
+++ backend/app/Http/Controllers/TipoAnimalController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TipoAnimal;
+
 
 class TipoAnimalController extends Controller
 {
@@ -12,6 +14,7 @@ class TipoAnimalController extends Controller
     public function index()
     {
         //
+        return response()->json(TipoAnimal::all());
     }
 
     /**
@@ -20,6 +23,16 @@ class TipoAnimalController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'nom_tipo' => 'required|max:100'
+        ]);
+
+        $tipo = TipoAnimal::create($request->all());
+
+        return response()->json([
+            'mensaje' => 'Tipo de animal creado exitosamente',
+            'tipo_animal' => $tipo
+        ], 201);
     }
 
     /**
@@ -28,6 +41,13 @@ class TipoAnimalController extends Controller
     public function show(string $id)
     {
         //
+        $tipo = TipoAnimal::find($id);
+
+        if (!$tipo) {
+            return response()->json(['mensaje' => 'Tipo de animal no encontrado'], 404);
+        }
+
+        return response()->json($tipo, 200);
     }
 
     /**
@@ -36,6 +56,22 @@ class TipoAnimalController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $tipo = TipoAnimal::find($id);
+
+        if (!$tipo) {
+            return response()->json(['mensaje' => 'Tipo de animal no encontrado'], 404);
+        }
+
+        $request->validate([
+            'nom_tipo' => 'required|max:100'
+        ]);
+
+        $tipo->update($request->all());
+
+        return response()->json([
+            'mensaje' => 'Tipo de animal actualizado exitosamente',
+            'tipo_animal' => $tipo
+        ], 200);
     }
 
     /**
@@ -44,5 +80,14 @@ class TipoAnimalController extends Controller
     public function destroy(string $id)
     {
         //
+        $tipo = TipoAnimal::find($id);
+
+        if (!$tipo) {
+            return response()->json(['mensaje' => 'Tipo de animal no encontrado'], 404);
+        }
+
+        $tipo->delete();
+
+        return response()->json(['mensaje' => 'Tipo de animal eliminado exitosamente'], 200);
     }
 }
